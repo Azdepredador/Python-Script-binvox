7a77c75cb0cf4c2a79a8045bed7bae21dbc593cc
Javier <franciscojavier950107@gmail.com>
2018-11-13 13:40:10 -0600

    Delete the first line in the file

--- Main.py
+++ Main.py
@@ -6,8 +6,14 @@ def runShell(obj):
     check_output("cd C:\Users\Prestador\Documents\Zernike & binvox.exe c:\Users\Prestador\Desktop\Objetos3D\\"+obj, shell=True)
     
 
+def readBinvox(binvox):
+    with open(binvox, 'r') as fin:
+        data = fin.read().splitlines(True)
+    with open(binvox, 'w') as fout:
+        fout.writelines(data[1:])
+
 if __name__ == "__main__":
     for f in glob.glob("*.binvox"):  
-        print(f)
-    
+        readBinvox(f)  
+      
     pass
