db7b6908c28c117336622550543d5ada9c8c3c6e
Javier <franciscojavier950107@gmail.com>
2018-11-13 12:18:53 -0600

    Run shell and commands in windows for binvox

---
+++ Main.py
@@ -0,0 +1,16 @@
+import glob
+from subprocess import check_output
+
+
+def runShell(obj):
+    #cmd=check_output("cd", shell=True) #return the path, where there are the objects
+    #print()
+    check_output("cd C:\Users\Prestador\Documents\Zernike & binvox.exe c:\Users\Prestador\Desktop\Objetos3D\\"+obj, shell=True)
+    
+
+if __name__ == "__main__":
+    for f in glob.glob("* "):
+        print(f)  
+   # runShell("lab ")
+    
+    pass
